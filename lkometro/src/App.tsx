85953d7ca76e03c49d74ab5ba8bfaeebacd81c1b
ASTITVA BHARDWAJ <bastitva0@gmail.com>
2026-06-21 15:27:06 +0530

    Wrap App component with ErrorBoundary

--- lkometro/src/App.tsx
+++ lkometro/src/App.tsx
@@ -2,23 +2,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { GoSmartCardProvider } from "@/contexts/GoSmartCardContext";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { ErrorBoundary } from "@/components/ErrorBoundary";
 import Index from "@/pages/Index";
 import NotFound from "@/pages/NotFound";
 
 function App() {
   return (
-    <ThemeProvider>
-      <GoSmartCardProvider>
-        <TooltipProvider delayDuration={200}>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
-        </TooltipProvider>
-      </GoSmartCardProvider>
-    </ThemeProvider>
+    <ErrorBoundary>
+      <ThemeProvider>
+        <GoSmartCardProvider>
+          <TooltipProvider delayDuration={200}>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </BrowserRouter>
+          </TooltipProvider>
+        </GoSmartCardProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
   );
 }
 
